eb1102df3399b0a28379701854e81ff97f436606
radixhound <radixhound@gmail.com>
2012-09-12 11:31:28 -0700

    Initial app with basic login
    New rails app with haml, sorcery and a basic sessions controller.
    Currently the login does not redirect to any particular page.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PunchId::Application.config.secret_token = '32ddb6248c0f0de7149256d7eba9478b6195d625a2010a207fe2ca5bb42b48121c83c7d9a25596ee6168bb22a3390c5dfeffb7f76e84d6f69dce1ebea402690b'
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+
+User.create!(username: 'admin',
+             email: 'admin@example.com',
+             password: 'letsgo')
